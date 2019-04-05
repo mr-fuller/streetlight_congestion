a3f622809b31d3b671979dd6da539b1b4341d47d
Mike Fuller <mrfuller460@gmail.com>
2019-04-05 17:37:07 -0400

    add createtable.sql and importcsv.sql

---
+++ importcsv.sql
@@ -0,0 +1,2 @@
+\copy secor_2016_01_seg_all 
+FROM 'C:/Users/fullerm/OneDrive - Toledo Metropolitan Area Council of Governments/Documents/Streetlight/secor/secor_segment_2016_01_1085_Travel/secor_segment_2016_1085_zone_seg_all.csv' csv header null as 'N/A';
